ef63c19931afb8977dddb055e950dd2cf2c0c9d1
Andreas Pardeike <andreas@pardeike.net>
2019-03-18 20:19:54 +0100

    initial commit

---
+++ ComponentUser/ComponentUser/ViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import MyComponent
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		_ = SomeClass("testing")
+	}
+}
---
+++ MyComponent/MyComponent/MyComponent.swift
@@ -0,0 +1,6 @@
+public class SomeClass {
+
+	public init(_ str: String) {
+		print("INIT SOMECLASS WITH \(str)")
+	}
+}
